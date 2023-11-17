612a6b243e882592f73c2a3d10e28356a512c859
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-11-17 05:52:47 +0100

    Creating input

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
+// My global vaiables
+let playerName;
+
+/**
+ * Removes startdiv
+ */
 function startGame() {
     document.querySelector(".startGame").style.display = "none";
 }
 
-
-
-let playerName;
-let input;
-
-
+/**
+*
+*
+*
+*/
 function getName() {
     playerName = document.getElementById("nameInput").value;
     console.log("hello");
     console.log(playerName);
     document.querySelector(".enterNameDiv").style.display = "none";
     document.querySelector(".playerName").innerText = playerName;
-}
-
-
-function getInput() {
-    var input = document.getElementById("getInput").value;
-    console.log(input);
+    // document.querySelector(".uppgift").innerText = "Välkommen till fångarna på fortet " + playerName;
 }
