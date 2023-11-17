a3f91fa3650720e0d550fad059a66b471d9f91ee
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-11-17 06:11:59 +0100

    Adding story

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 // My global vaiables
 let playerName;
 
-/**
- * Removes startdiv
- */
-function startGame() {
-    document.querySelector(".startGame").style.display = "none";
-}
 
-/**
-*
-*
-*
-*/
-function getName() {
-    playerName = document.getElementById("nameInput").value;
-    console.log("hello");
+
+ function getName() {
     console.log(playerName);
     document.querySelector(".enterNameDiv").style.display = "none";
     document.querySelector(".playerName").innerText = playerName;
-    // document.querySelector(".uppgift").innerText = "Välkommen till fångarna på fortet " + playerName;
+    document.querySelector(".uppgift").innerText = "Välkommen till fångarna på fortet " + playerName + "! Du står nu framför en stor och tung dörr";
+    document.querySelector(".val").innerText = "För att öppna säg: öppna";
 }
