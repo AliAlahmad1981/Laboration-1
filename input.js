3fabdacf3d2ab29e21bec41eca2eba53c7a8fa32
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2023-11-17 06:24:09 +0100

    Adding second game

--- input.js
+++ input.js
@@ -1,33 +1,37 @@
 function getInput() {
     input = document.querySelector("#getInput").value;
     console.log(input);
-    const uppgiften = document.querySelector(".uppgift");
-    const valen = document.querySelector(".val");
-    const getInput = document.querySelector(".getInput");
-    const getInputGame = document.querySelector(".getInputGame");
 
     switch (input) {
         case "öppna":
             uppgiften.innerText = "Välkommen till fortet " + playerName + "!" + " Vågar du gå vidare?/Gunde";
-            valen.innerText = "För att gå vidare säg: vidare";
+            valen.innerText = "För att gå vidare skriv: vidare";
+            document.querySelector("#getInput").value = "";
             break;
         case "vidare":
             uppgiften.innerText = "Du har kommit till ditt första rum " + playerName + " /Gunde";
-            valen.innerText = "För att gå in i första rummet säg: rum1";
+            valen.innerText = "För att gå in i första rummet skriv: rum1";
+            document.querySelector("#getInput").value = "";
             break;
         case "rum1":
-            getInput.style.display = "none";
-            getInputGame.style.display = "flex";
-            uppgiften.innerText = "Du ska leta efter en nyckel i lådorna /Gunde Du ska ta ett nummer som kan blir 36 om du tar nummret gånger sig själv du har tre försök";
-            valen.innerText = "Välj att svara mellan något nummer mellan : 1-6";
+            printInput.style.display = "none";
+            printInputGame.style.display = "flex";
+            uppgiften.innerText = "Här ska du gissa på ett nummer mellan 1 och 100 " +
+            "Du får veta om det är för högt eller för lågt. Du har sex chanser. Lycka till/Gunde";
+            valen.innerText = "Välj ett numer mellan : 1-100";
             break;
         case "hej":
             uppgiften.innerText = "Hej " + playerName;
-            break;
-        case "?":
-            uppgiften.innerText = "Är du förvirrad " + playerName;
-            break;
-        default:
-            uppgiften.innerText = "Jag förstår inte vad du menar...testa igen";
+	 function getInput() {
     }
 }
+
+
+
+// case "rum1":
+//         printInput.style.display = "none";
+//         printInputGame.style.display = "flex";
+//         uppgiften.innerText = "Du ska leta efter en nyckel i lådorna! " +
+//         "Du ska välja ett nummer som kan blir 36 om du tar detta nummret gånger sig själv du har tre försök";
+//         valen.innerText = "Välj att svara något nummer mellan : 1-6";
+//         break;
